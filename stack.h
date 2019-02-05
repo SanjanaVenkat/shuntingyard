f9e663507226669a51791bb6337d4faa047b92da
SanjanaVenkat <39311548+SanjanaVenkat@users.noreply.github.com>
2019-02-05 10:43:45 -0800

    Add files via upload

---
+++ stack.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Stack {
+ public:
+  Stack();
+  ~Stack();
+  bool isEmpty();
+  bool isFull();
+  int push();
+  int pop();
+  int peek();
+ private:
+
+
+};
